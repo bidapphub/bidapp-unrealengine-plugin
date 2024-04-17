b7a6d14c63eed80a884ae73bad58dda5614f4600
DimaShpikliryi <148830475+DimaShpikliryi@users.noreply.github.com>
2024-04-17 17:26:53 +0300

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    alias(libs.plugins.androidApplication).apply(false)
-    alias(libs.plugins.androidLibrary).apply(false)
-    alias(libs.plugins.kotlinAndroid).apply(false)
-    alias(libs.plugins.kotlinMultiplatform).apply(false)
-    alias(libs.plugins.kotlinCocoapods).apply(false)
-    alias(libs.plugins.composemulti).apply(false)
-}
